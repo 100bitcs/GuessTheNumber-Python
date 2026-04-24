5adafc55544784084258aec5e88820e4c51db882
100bitcs <shobhitpainuly888@gmail.com>
2026-04-25 03:00:08 +0530

    For 1 to 1000

---
+++ range_large.py
@@ -0,0 +1,21 @@
+import random
+
+#~ For large range of 1 - 1000.
+
+def rng_lar():
+    num=random.randrange(1,1001)
+    count=0
+    while True:
+        ans=int(input("GUESS THE NUMBER: "))
+        count+=1
+        if ans >=1001:
+            print("KNOW YOUR PLACE!!")
+        else:
+            if ans > num:
+                print("LOOK BACK!!")
+            elif ans<num:
+                print("UP THERE!!")
+            elif ans ==num:
+                print("YOU GOT IT!!")
+                break
+    print(f"YOU HAVE TAKEN {count} GUESSES.")
